6d0a019f22dee24cb768f59fb1057f4c5e3e2ae3
Yunzhi Liu <liu_yun_zhi@163.com>
2018-08-05 23:24:38 +0800

    [liuyunzhi] feature: add update a product API

--- src/main/java/com/thoughtworks/mallbackend/controller/ProductController.java
+++ src/main/java/com/thoughtworks/mallbackend/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,5 +31,11 @@ public class ProductController {
     public ResponseEntity add(@RequestBody Product product) {
         Long id = productService.add(product);
         return ResponseEntity.created(URI.create("/products/" + id)).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody Product product) {
+        productService.update(id, product);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/thoughtworks/mallbackend/service/ProductService.java
+++ src/main/java/com/thoughtworks/mallbackend/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.mallbackend.service;
 
 import com.thoughtworks.mallbackend.entity.Product;
+import com.thoughtworks.mallbackend.exception.ProductNotFoundException;
 import com.thoughtworks.mallbackend.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,18 @@ public class ProductService {
 
     public Long add(Product product) {
         return productRepository.save(product).getId();
+    }
+
+
+    public void update(Long id, Product product) {
+        Product oldProduct = productRepository.findById(id).orElseThrow(ProductNotFoundException::new);
+        oldProduct.setName(product.getName());
+        oldProduct.setPrice(product.getPrice());
+        oldProduct.setUnit(product.getUnit());
+        oldProduct.setImage(product.getImage());
+        oldProduct.setDescription(product.getDescription());
+        oldProduct.setProductionDate(product.getProductionDate());
+        oldProduct.setProductionPlace(product.getProductionPlace());
+        productRepository.save(oldProduct);
     }
 }
